d10612076a87953f9de008e1de34a0bb6cb49fdd
kitty841002@gmai.com <kitty841002@gmai.com>
2024-12-17 00:45:03 +0800

    綠勾勾完成

---
+++ js/check-anima.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  // 取得元素
+  const circle = document.querySelector('.circle');
+  const tick = document.querySelector('.tick');
+
+  // 啟動動畫
+  circle.style.strokeDashoffset = 0; // 圓圈繪製動畫
+  tick.style.strokeDashoffset = 0; // 勾繪製動畫
+};
